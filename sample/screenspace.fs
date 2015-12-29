7e2de6bb896a75cc0312c1321f381a796ac2bc34
Avishkar Autar <aautar@digital-radiation.com>
2015-12-29 17:14:23 -0500

    better error reporting, updated sample to do simple blur + periodic oversaturate

--- sample/screenspace.fs
+++ sample/screenspace.fs
@@ -1,24 +1,28 @@
 precision mediump float;
 
+uniform float uPeriod;
 uniform sampler2D uSampler;		  
 varying vec2 vTextureCoord;
 
 void main(void) {
 
-
     vec4 sum = vec4( 0. );
-    vec2 inc = vec2(0.002);
+    float px = (1.0 / 800.0) * 2.5;
 
-    sum += texture2D( uSampler, ( vTextureCoord - inc * 4. ) ) * 0.051;
-    sum += texture2D( uSampler, ( vTextureCoord - inc * 3. ) ) * 0.0918;
-    sum += texture2D( uSampler, ( vTextureCoord - inc * 2. ) ) * 0.12245;
-    sum += texture2D( uSampler, ( vTextureCoord - inc * 1. ) ) * 0.1531;
-    sum += texture2D( uSampler, ( vTextureCoord + inc * 0. ) ) * 0.1633;
-    sum += texture2D( uSampler, ( vTextureCoord + inc * 1. ) ) * 0.1531;
-    sum += texture2D( uSampler, ( vTextureCoord + inc * 2. ) ) * 0.12245;
-    sum += texture2D( uSampler, ( vTextureCoord + inc * 3. ) ) * 0.0918;
-    sum += texture2D( uSampler, ( vTextureCoord + inc * 4. ) ) * 0.051;
+    vec4 src = texture2D( uSampler, ( vTextureCoord ) );
 
-    gl_FragColor = sum;
+    sum += texture2D( uSampler, ( vTextureCoord + vec2(-px, 0) ) );
+    sum += texture2D( uSampler, ( vTextureCoord + vec2(-px, -px) ) );
+    sum += texture2D( uSampler, ( vTextureCoord + vec2(0, -px) ) );
+    sum += texture2D( uSampler, ( vTextureCoord + vec2(px, -px) ) );
+    sum += texture2D( uSampler, ( vTextureCoord + vec2(px, 0) ) );
+    sum += texture2D( uSampler, ( vTextureCoord + vec2(px, px) ) );
+    sum += texture2D( uSampler, ( vTextureCoord + vec2(0, px) ) );
+    sum += texture2D( uSampler, ( vTextureCoord + vec2(-px, px) ) );
+    sum += src;
+
+    sum = sum / 9.0;
+
+    gl_FragColor = src + (sum * 2.5 * uPeriod);
 
 }
